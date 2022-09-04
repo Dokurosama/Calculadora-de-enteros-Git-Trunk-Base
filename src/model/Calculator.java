4b12b1f07281f207177b78ee5721d1ff71609c13
IvanMCvu <109694555+IvanMCvu@users.noreply.github.com>
2022-09-03 22:50:44 -0500

    Método de multiplicación
    Se realizo el método de multiplicación entre dos números, pasando como parámetros, primero el multiplicando y luego el multiplicador

--- src/model/Calculator.java
+++ src/model/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
 	 * Metodo para realizar la multiplicacion de dos numeros
 	 * @param numberOne, es el numero que representa al multiplicando
 	 * @param numberTwo, es el numero que representa el multiplicador
-	 * @return es el resultado de la multiplicacion, es decir, el producto
+	 * @return es el producto de la multiplicacion
 	 */
 	public int multiplication(int numberOne, int numberTwo) {
 		return numberOne * numberTwo;
